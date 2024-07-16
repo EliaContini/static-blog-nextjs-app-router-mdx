aaaff49a8f717235c75fb7c442c8093cbb08e7e3
Elia Contini <450179+EliaContini@users.noreply.github.com>
2024-07-16 14:27:03 +0200

    chore: formatting code

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,4 @@ export const SITE_PUBLISHER = "Elia Contini";
 export const SITE_DESCRIPTION =
   "A blog built with Next.js 14, App Router and MDX";
 export const SITE_TITLE = "Next.js 14 Static Blog";
-
-
 export const SITE_URL = "https://www.example.com";
